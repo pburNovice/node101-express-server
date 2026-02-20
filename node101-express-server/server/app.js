b69e0712becfeb411b852b766e2e80c858cd9359
pburNovice <patburlaza1@gmail.com>
2026-02-20 15:12:08 -0800

    Add files via upload

---
+++ node101-express-server/server/app.js
@@ -0,0 +1,19 @@
+// import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json')
+
+// create your express server below
+const app = express();
+
+// add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', (req,res) => {
+    res.send("It's working");
+});
+app.get('/data', (req,res)=> {
+    res.json(data)
+});
+// finally export the express application
+module.exports = app;
